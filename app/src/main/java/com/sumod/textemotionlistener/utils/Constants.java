fe0fd08d50985342268b7c0bf1130f11c91e847b
Sumod Kulkarni <sumodkulkarni@users.noreply.github.com>
2016-09-11 03:25:16 +0530

    Update Constants.java

--- app/src/main/java/com/sumod/textemotionlistener/utils/Constants.java
+++ app/src/main/java/com/sumod/textemotionlistener/utils/Constants.java
@@ -5,6 +5,6 @@ package com.sumod.textemotionlistener.utils;
  */
 public class Constants {
     public static final String watson_URL = "https://gateway.watsonplatform.net/tone-analyzer/api";
-    public static final String watson_username = "f6897a12-8ee8-4d58-af36-2f4bb9ded0a7";
-    public static final String watson_password = "mljuoy4poTI3";
+    public static final String watson_username = "enter-username-here";
+    public static final String watson_password = "enter-password-here";
 }
